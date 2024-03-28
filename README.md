cd680834f7669b91a0f901bff31808175ddf1b01
mathieura <mathieura@mathieu.home>
2024-03-28 19:57:34 +0100

    feat: initial version

---
+++ README.md
@@ -0,0 +1,26 @@
+# Etherpad Management Tool
+
+## Description
+The Etherpad Management Tool is a Bash script designed to facilitate the management of Etherpad instances on CPanel servers.
+
+## Features
+- Install and configure Etherpad instances
+- Start Etherpad instances
+- Proxy setup for Etherpad access
+
+## Requirements
+- Bash shell
+- NodeJS application on CPanel
+
+## Installation
+1. Clone or download this repository to your local machine.
+2. Make sure you have all the requirements installed on your system.
+3. Run the script with appropriate permissions: `./ether-tool.sh || bash ether-tool.sh`
+
+## Usage
+- When prompted, select whether you want to install or start an Etherpad instance.
+- Provide the necessary information such as the root application folder, hostname, and username.
+- Follow the on-screen instructions to complete the installation or start process.
+
+## Contributing
+Contributions are welcome! If you'd like to contribute to this project, please fork the repository and submit
---
+++ ether-tool.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Project Name: Etherpad Management Tool
+# Description: Bash script for managing Etherpad on CPanel.
+# Author: Mathieu R
+# License: MIT License
+# GitHub: https://github.com/MathieuRA/ether-tool
+
+# Etherpad settings
+folder_name="etherpad"
+
+# Etherpad settings
+folder_name="etherpad"
+
+echo "Bienvenue dans l'outil de gestion Etherpad >=2.0.0 pour CPanel"
+echo "Installation(i)/Démarrage(d)"
+read -r method
+
+echo "Nom du dossier d'application root"
+read -r folder
+
+echo "Nom d'hôte :"
+read -r hostname
+
+echo "Nom d'utilisateur :"
+read -r username
+
+install_etherpad() {
+    local folder="$1"
+    local folder_name="$2"
+    local version="2.0.1"
+    local extracted_folder_name="etherpad-lite-$version"
+    local filename="v$version.zip"
+
+    echo "Activation de l'environnement NodeJS"
+    source /home/sc4abpa0193/nodevenv/"$folder"/20/bin/activate && cd /home/sc4abpa0193/"$folder"
+
+    echo "Mise en place du proxy pour etherpad"
+    npm install http-proxy
+    echo "const proxy = require('http-proxy');" > proxy.js
+    echo "proxy.createProxyServer({ target: 'http://localhost:9001' }).listen(8080);" >> proxy.js
+
+    pwd
+    echo "Téléchargement de $extracted_folder_name"
+    wget "https://github.com/ether/etherpad-lite/archive/refs/tags/$filename"
+
+    echo "Extraction des fichiers de l'archive téléchargée"
+    unzip "$filename"
+    rm "$filename"
+
+    echo "Renommage du dossier $extracted_folder_name -> $folder_name"
+    mv "$extracted_folder_name" "$folder_name"
+    cd "$folder_name"
+
+    echo "Installation de l'arbre des dépendances"
+    bin/installDeps.sh
+
+    echo "Démarrage du logiciel"
+    pnpm run prod "$@"
+}
+
+start_etherpad() {
+    local folder="$1"
+    local folder_name="$2"
+
+    echo "Activation de l'environnement NodeJS"
+    source /home/sc4abpa0193/nodevenv/"$folder"/20/bin/activate && cd /home/sc4abpa0193/"$folder"
+
+    cd "$folder_name"
+
+    echo "Démarrage du logiciel"
+    pnpm run prod "$@"
+}
+
+if [ "$method" = "i" ]; then
+    ssh "$username@$hostname" "$(typeset -f install_etherpad); install_etherpad \"$folder\" \"$folder_name\""
+elif [ "$method" = "d" ]; then
+    ssh "$username@$hostname" "$(typeset -f start_etherpad); start_etherpad \"$folder\" \"$folder_name\""
+else
+    echo "Méthode inconnue : $method"
+fi
